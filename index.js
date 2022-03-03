d63076b3915b7c2c49028572fc4ddf9a961a10a1
CrypticSignal <theaudiophile@outlook.com>
2022-03-03 21:45:35 +0000

    Clear the input box after adding a task

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const enterButton= document.getElementById("enter");
-const input= document.getElementById("input");
-const list= document.querySelector('ul');
+const enterButton = document.getElementById("enter");
+const input = document.getElementById("input");
+const list = document.querySelector("ul");
 
-const tasks = document.querySelector("li")
+const tasks = document.querySelector("li");
 // console.log (enterButton, input, list, item);
 
-
-function lengthOfInput(){
-    return input.value.length;
+function lengthOfInput() {
+  return input.value.length;
 }
 
-function listElement (){
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    list.appendChild(li);
+function listElement() {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  list.appendChild(li);
 
+  function crossOutTask() {
+    li.classList.toggle("done");
+  }
+  li.addEventListener("click", crossOutTask);
 
-    function crossOutTask () {
-        li.classList.toggle('done');
-    }
-    li.addEventListener("click", crossOutTask);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.appendChild(document.createTextNode("X"));
-    li.appendChild(deleteBtn);
-    deleteBtn.addEventListener("click", deleteListItem);
-    function deleteListItem () {
-        li.classList.add("delete");
-        setTimeout(function () {
-            li.style.display = "none";
-        }, 500);
-    }
+  const deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("X"));
+  li.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", deleteListItem);
+  function deleteListItem() {
+    li.classList.add("delete");
+    setTimeout(function () {
+      li.style.display = "none";
+    }, 500);
+  }
 }
 
 // listElement();
 
-function addTasksWhenClick (){
-    if (lengthOfInput() > 0){
-        listElement();
-    }
+function addTasksWhenClick() {
+  if (lengthOfInput() > 0) {
+    listElement();
+    input.value = "";
+  }
 }
 
 enterButton.onclick = () => addTasksWhenClick();
 
-input.addEventListener('keypress', ()=>{
-    if(lengthOfInput()>0 && event.which===13){
-        listElement();
-    }
-})
+input.addEventListener("keypress", () => {
+  if (lengthOfInput() > 0 && event.which === 13) {
+    listElement();
+    input.value = "";
+  }
+});
